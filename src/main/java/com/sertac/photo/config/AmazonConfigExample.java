2ff58067e3f60c8dc12340f1931bfddcdbb03ecb
Sertaç Savaş <sertacsavas@hotmail.com>
2020-01-09 21:51:07 +0300

    config examples

---
+++ src/main/java/com/sertac/photo/config/AmazonConfigExample.java
@@ -0,0 +1,17 @@
+package com.sertac.photo.config;
+
+//@Configuration
+public class AmazonConfigExample {
+
+//	@Configuration
+//	public class AmazonConfig {
+//		@Bean
+//		public AmazonS3 s3() {
+//			AWSCredentials awsCredentials = new BasicAWSCredentials("??",
+//					"??");
+//			return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+//					.withRegion("eu-central-1").build();
+//		}
+//
+//	}
+}
