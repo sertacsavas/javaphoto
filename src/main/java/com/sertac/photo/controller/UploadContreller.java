84dd7629439c13e54661ae027c143d054119fdfd
Sertaç Savaş <sertacsavas@hotmail.com>
2020-01-10 00:27:13 +0300

    upload photo

--- src/main/java/com/sertac/photo/controller/UploadContreller.java
+++ src/main/java/com/sertac/photo/controller/UploadContreller.java
@@ -1,10 +1,10 @@
 package com.sertac.photo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,8 +19,12 @@ public class UploadContreller {
 	@Autowired
 	UploadService uploadService;
 
-	@PostMapping(path = "/media/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public void upload(@CurrentUser UserPrincipal currentUser, @RequestParam("media") MultipartFile media) {
+	@PostMapping(path = "/media/upload"/*
+										 * , consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces =
+										 * MediaType.APPLICATION_JSON_VALUE
+										 */)
+	@ResponseBody
+	public void upload(@CurrentUser UserPrincipal currentUser, @RequestParam("file") MultipartFile media) {
 		uploadService.upload(currentUser.getId(), media);
 
 	}
